62424f56cc02500e3e17be56600488e34a30e924
Mamosebetsi Kekae <mkekae@gmail.com>
2022-06-08 19:35:19 -0700

    added cra and tailwind css and fonts

---
+++ my-web/src/App.js
@@ -0,0 +1,16 @@
+
+import { Fragment } from 'react';
+//import Nav from 'components/nav/Nav';
+import Nav from 'components/nav/Nav'
+
+function App() {
+	return (
+		<Fragment>
+			
+		</Fragment>
+	);
+}
+
+export default App;
+
+// <Nav />;
